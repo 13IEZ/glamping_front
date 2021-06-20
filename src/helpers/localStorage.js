e3011e42add92d529365a753641e01e8f3e1df78
Igor <zahik1311@gmail.com>
2021-06-20 12:59:28 +0600

    front-feature #5 add dev dep husky, lint-staged, prettier, eslint and types for TS and react. Refresh .gitignore files. Add protect to commit in main branch

--- src/helpers/localStorage.js
+++ src/helpers/localStorage.js
@@ -1,14 +1,14 @@
 export const getUserFromLocalStorage = () => {
   const state = localStorage.getItem('state');
 
-  if (!!state) {
+  if (state) {
     return JSON.parse(state);
   }
 
   return undefined;
 };
 
-export const setUserToLocalStorage = (data) => {
+export const setUserToLocalStorage = data => {
   const state = JSON.stringify(data);
   localStorage.setItem('state', state);
 };
