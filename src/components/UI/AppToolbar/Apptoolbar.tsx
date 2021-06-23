8a24c80d659614818566d2877083a82e5fef9086
Pavel Khegay <pashulia.pk@gmail.com>
2021-06-23 13:14:09 +0600

    feature#7 added userMenu, anonymousMenu and appToolbar

---
+++ src/components/UI/AppToolbar/Apptoolbar.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {
+  Layout,
+  Menu
+} from 'antd';
+import { useSelector } from 'react-redux';
+
+import AnonymousMenu from './Menus/AnonymousMenu';
+import UserMenu from './Menus/UserMenu';
+
+const { Header } = Layout;
+
+const AppToolbar: React.FC = () => {
+  const user = useSelector(state => state.users.user);
+
+  return (
+    <Layout>
+    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+      {
+        user ? <UserMenu user={user}/> : <AnonymousMenu />
+      }
+      </Menu>
+    </Header>
+    </Layout>
+  )
+}
+
+export default AppToolbar;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
+import './index.css';
+import 'antd/dist/antd.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import { ConnectedRouter } from 'connected-react-router';
-import store, { history } from './store/configureStore';
 import { Provider } from 'react-redux';
+
+import App from './App';
+import store, { history } from './store/configureStore';
 
 const app = (
   <Provider store={store}>
@@ -17,7 +20,3 @@ const app = (
 
 ReactDOM.render(app, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
