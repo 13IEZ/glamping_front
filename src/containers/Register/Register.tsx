b5ed530b93093e6c1fe2415317a00367048e5a31
Dauren3310 <daur3310@gmail.com>
2021-06-24 15:44:09 +0600

    front-feadure #12 created form (sing up) with antd in file register.tsx

--- src/containers/Register/Register.tsx
+++ src/containers/Register/Register.tsx
@@ -1,34 +1,32 @@
-import React, {useState, useEffect} from 'react';
-import {Form, Input, Button} from 'antd';
+import {Form, Input, Button, Checkbox} from 'antd';
 import {UserOutlined, LockOutlined} from '@ant-design/icons';
+import './Register.css';
 
-const Register: React.FC = () => {
-
-  const [form] = Form.useForm();
-  const [, forceUpdate] = useState({});
-
-  // To disable submit button at the beginning.
-  useEffect(() => {
-    forceUpdate({});
-  }, []);
-
+const Register = () => {
   const onFinish = (values: any) => {
-    console.log('Finish:', values);
+    console.log('Received values of form: ', values);
   };
 
   return (
-    <div>
+    <div className='Register'>
 
-      <Form form={form} name="horizontal_login" layout="inline" onFinish={onFinish}>
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{remember: true}}
+        onFinish={onFinish}
+      >
+        <h2 className='Register-header'>Sign Up</h2>
+
         <Form.Item
           name="username"
-          rules={[{required: true, message: 'Please input your username!'}]}
+          rules={[{required: true, message: 'Please input your Username!'}]}
         >
           <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Username"/>
         </Form.Item>
         <Form.Item
           name="password"
-          rules={[{required: true, message: 'Please input your password!'}]}
+          rules={[{required: true, message: 'Please input your Password!'}]}
         >
           <Input
             prefix={<LockOutlined className="site-form-item-icon"/>}
@@ -36,23 +34,26 @@ const Register: React.FC = () => {
             placeholder="Password"
           />
         </Form.Item>
-        <Form.Item shouldUpdate>
-          {() => (
-            <Button
-              type="primary"
-              htmlType="submit"
-              disabled={
-                !form.isFieldsTouched(true) ||
-                !!form.getFieldsError().filter(({errors}) => errors.length).length
-              }
-            >
-              Log in
-            </Button>
-          )}
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Sign Up
+          </Button>
+          Or <a href="">Log In now!</a>
         </Form.Item>
       </Form>
 
     </div>
+
   );
 };
 
