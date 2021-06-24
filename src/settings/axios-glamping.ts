8ab8a20012b2ee189bd495a81b61dd482b4143cc
Dauren3310 <daur3310@gmail.com>
2021-06-24 14:23:47 +0600

    front-bag in file axios-glamping.ts added type for req

--- src/settings/axios-glamping.ts
+++ src/settings/axios-glamping.ts
@@ -1,13 +1,16 @@
+// @ts-ignore
 import axios from 'axios';
 import config from './config';
 import store from '../store/configureStore';
 import { UserState } from '../store/types/users';
 
+
+
 const ax = axios.create({
   baseURL: config.apiUrl,
 });
 
-ax.interceptors.request.use(req => {
+ax.interceptors.request.use((req: { headers: { [x: string]: string; }; }) => {
   const users: UserState = store.getState().users;
   if (users.user) req.headers['Authentication'] = users.user.token;
   return req;
