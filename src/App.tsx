454a619395bb543e6e6f9e68650acb757da2d59f
Dauren3310 <daur3310@gmail.com>
2021-06-24 14:57:04 +0600

    front-feature #12 installed types for react-router-dom added components - (Switch and Route) to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Register from "./containers/Register/Register";
 
 function App() {
 
 
   return (
     <div className='App'>
+      <Switch>
+        <Route path='/register' component={Register} exact />
+      </Switch>
       <header className='App-header'>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
--- src/settings/axios-glamping.ts
+++ src/settings/axios-glamping.ts
@@ -5,12 +5,11 @@ import store from '../store/configureStore';
 import { UserState } from '../store/types/users';
 
 
-
 const ax = axios.create({
   baseURL: config.apiUrl,
 });
 
-ax.interceptors.request.use((req: { headers: { [x: string]: string; }; }) => {
+ax.interceptors.request.use((req: any) => {
   const users: UserState = store.getState().users;
   if (users.user) req.headers['Authentication'] = users.user.token;
   return req;
