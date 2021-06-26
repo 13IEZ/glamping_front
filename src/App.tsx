069d6dfd60147f90400dec9b4e6e528f26c00963
Diana Isseyeva <cherepashka.d74@gmail.com>
2021-06-26 22:09:41 +0600

    front-feature #36 created page Main and block for landlord

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import AppToolbar from './components/UI/AppToolbar/AppToolbar';
 import ProtectedRoute from './helpers/ProtectedRoute';
 import { Switch } from 'react-router';
 import { useTypedSelectorHook } from './hooks/useTypedSelector';
+import Main from './pages/Main/Main';
 
 function App(): any {
   const { user } = useTypedSelectorHook(state => state.users);
@@ -14,6 +15,7 @@ function App(): any {
       <Switch>
         <ProtectedRoute isAllowed={!user} path='/register' exact redirectTo='/' component={Register} />
       </Switch>
+      <Main/>
     </>
   );
 }
