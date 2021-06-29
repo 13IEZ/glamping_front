f1900d8ed6ef0ec186092397f1f21da2f116e9bc
Pavel Khegay <pashulia.pk@gmail.com>
2021-06-29 13:49:05 +0600

    added the reviews block, registered a form for sending new reviews, created a separate page with all reviews

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import Register from './pages/Register/Register';
+
+import { Layout } from 'antd';
+import {
+  Route,
+  Switch,
+} from 'react-router';
+
 import ProtectedRoute from './helpers/ProtectedRoute';
-import { Switch, Route } from 'react-router';
 import { useTypedSelectorHook } from './hooks/useTypedSelector';
 import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
-import { Layout } from 'antd';
+import Register from './pages/Register/Register';
+import ReviewForm from './pages/ReviewForm/ReviewForm';
+import Reviews from './pages/Reviews/Reviews';
 
 function App(): JSX.Element {
   const { user } = useTypedSelectorHook(state => state.users);
@@ -14,8 +21,10 @@ function App(): JSX.Element {
     <Layout style={{ backgroundColor: '#fff' }}>
       <Route path='/' exact component={Main} />
       <Switch>
-        <ProtectedRoute isAllowed={!user} path='/register' exact redirectTo='/' component={Register} />
-        <ProtectedRoute isAllowed={!user} path='/login' exact redirectTo='/' component={Login} />
+        <ProtectedRoute isAllowed={true} path='/register' exact redirectTo='/' component={Register} />
+        <ProtectedRoute isAllowed={true} path='/login' exact redirectTo='/' component={Login} />
+        <ProtectedRoute isAllowed={true} path='/reviews' exact redirectTo='/' component={Reviews } />
+        <ProtectedRoute isAllowed={true} path='/reviews/new' exact redirectTo='/' component={ReviewForm } />
       </Switch>
     </Layout>
   );
