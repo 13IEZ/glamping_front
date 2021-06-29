06716cae1f96c126ef759bdcb98d3b2438b9ec89
Vladislavsavi <vladislavsavi@gmail.com>
2021-06-29 20:31:12 +0600

    front-test #61 add TODO

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
 import { Layout } from 'antd';
-import {
-  Route,
-  Switch,
-} from 'react-router';
+import { Route, Switch } from 'react-router';
 
 import ProtectedRoute from './helpers/ProtectedRoute';
-import { useTypedSelectorHook } from './hooks/useTypedSelector';
+// import { useTypedSelectorHook } from './hooks/useTypedSelector';
 import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
 import Register from './pages/Register/Register';
@@ -15,16 +12,17 @@ import ReviewForm from './pages/ReviewForm/ReviewForm';
 import Reviews from './pages/Reviews/Reviews';
 
 function App(): JSX.Element {
-  const { user } = useTypedSelectorHook(state => state.users);
-
+  // Раскоментить при необходимости
+  // const { user } = useTypedSelectorHook(state => state.users);
+  // TODO Создать компонент Layout в для отображения одной шапки на всех страницах
   return (
     <Layout style={{ backgroundColor: '#fff' }}>
       <Route path='/' exact component={Main} />
       <Switch>
         <ProtectedRoute isAllowed={true} path='/register' exact redirectTo='/' component={Register} />
         <ProtectedRoute isAllowed={true} path='/login' exact redirectTo='/' component={Login} />
-        <ProtectedRoute isAllowed={true} path='/reviews' exact redirectTo='/' component={Reviews } />
-        <ProtectedRoute isAllowed={true} path='/reviews/new' exact redirectTo='/' component={ReviewForm } />
+        <ProtectedRoute isAllowed={true} path='/reviews' exact redirectTo='/' component={Reviews} />
+        <ProtectedRoute isAllowed={true} path='/reviews/new' exact redirectTo='/' component={ReviewForm} />
       </Switch>
     </Layout>
   );
