911cbd45be5bb1077321c45b61fce80131c9a70a
Igor <zahik1311@gmail.com>
2021-06-30 22:06:53 +0600

    front-feature #57 part1 - add assets folder, _mixins.scss. Improved AboutUs, Investor, ReviewsMini, UserMenu .tsx. Delete AboutUs.scss

--- src/components/ReviewsMini/ReviewsMini.tsx
+++ src/components/ReviewsMini/ReviewsMini.tsx
@@ -1,11 +1,6 @@
 import './ReviewsMini.scss';
 
-import {
-  Card,
-  Col,
-  Row,
-  Typography,
-} from 'antd';
+import { Card, Col, Row, Typography } from 'antd';
 import { Link } from 'react-router-dom';
 import React from 'react';
 
@@ -18,37 +13,43 @@ const ReviewsMini: React.FC = () => {
 
   return (
     <section style={{ padding: '0 50px' }}>
-      <div className="Reviews">
-      <Title level={3}>Отзывы</Title>
+      <div className='Reviews'>
+        <Title level={3}>Отзывы</Title>
         <Row gutter={16}>
           <Col span={8}>
-            <Card title="Иванов Иван" style={{ width: 300 }} >
+            <Card title='Иванов Иван' style={{ width: 300 }}>
               <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit hic architecto maxime quam placeat
-                temporibus! Sapiente, dolores inventore dolorem blanditiis quae voluptate dolorum non. Sed nam minima quas?
-                Quod, rem...
+                temporibus! Sapiente, dolores inventore dolorem blanditiis quae voluptate dolorum non. Sed nam minima
+                quas? Quod, rem...
               </p>
-              <Link to='/reviews' onClick={clickHandler}>Читать полностью</Link>
+              <Link to='/reviews' onClick={clickHandler}>
+                Читать полностью
+              </Link>
             </Card>
           </Col>
           <Col span={8}>
-            <Card title="Иванов Иван" style={{ width: 300 }}>
+            <Card title='Иванов Иван' style={{ width: 300 }}>
               <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit hic architecto maxime quam placeat
-                temporibus! Sapiente, dolores inventore dolorem blanditiis quae voluptate dolorum non. Sed nam minima quas?
-                Quod, rem...
+                temporibus! Sapiente, dolores inventore dolorem blanditiis quae voluptate dolorum non. Sed nam minima
+                quas? Quod, rem...
               </p>
-              <Link to='/reviews' onClick={clickHandler}>Читать полностью</Link>
+              <Link to='/reviews' onClick={clickHandler}>
+                Читать полностью
+              </Link>
             </Card>
           </Col>
           <Col span={8}>
-            <Card title="Иванов Иван" style={{ width: 300 }}>
+            <Card title='Иванов Иван' style={{ width: 300 }}>
               <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit hic architecto maxime quam placeat
-                temporibus! Sapiente, dolores inventore dolorem blanditiis quae voluptate dolorum non. Sed nam minima quas?
-                Quod, rem...
+                temporibus! Sapiente, dolores inventore dolorem blanditiis quae voluptate dolorum non. Sed nam minima
+                quas? Quod, rem...
               </p>
-              <Link to='/reviews' onClick={clickHandler}>Читать полностью</Link>
+              <Link to='/reviews' onClick={clickHandler}>
+                Читать полностью
+              </Link>
             </Card>
           </Col>
         </Row>
