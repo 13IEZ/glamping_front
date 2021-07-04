dec85ab6a99505b1f102a869b3e11918096f5f74
Pavel Khegay <pashulia.pk@gmail.com>
2021-07-04 14:46:44 +0600

    front-feature #67 removed div tag on pages BuyModuleForm & Login

--- src/pages/BuyModuleForm/BuyModuleForm.tsx
+++ src/pages/BuyModuleForm/BuyModuleForm.tsx
@@ -2,11 +2,7 @@ import './Register.scss';
 
 import React, { useState } from 'react';
 
-import {
-  Button,
-  Form,
-  Input,
-} from 'antd';
+import { Button, Form, Input } from 'antd';
 
 import { LockOutlined } from '@ant-design/icons';
 
@@ -31,26 +27,24 @@ const BuyModuleForm: React.FC = () => {
   };
 
   return (
-    <div className='Form'>
-      <Form name='normal_login' className='login-form' initialValues={{ remember: true }} onFinish={submitFormHandler}>
-        <h2 className='Form-header'>Регистрация</h2>
-        <Form.Item name='phone' rules={[{ required: true, message: 'Пожалуйста, укажите корректный номер!' }]}>
-          <Input
-            name='phone'
-            className='form_input'
-            prefix={<LockOutlined className='site-form-item-icon' />}
-            type='phone'
-            placeholder='Укажите ваш сотовый'
-            onChange={inputChangeHandler}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button type='primary' htmlType='submit' className='login-form-button'>
-            Регистрация
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+    <Form name='normal_login' className='login-form' initialValues={{ remember: true }} onFinish={submitFormHandler}>
+      <h2 className='Form-header'>Регистрация</h2>
+      <Form.Item name='phone' rules={[{ required: true, message: 'Пожалуйста, укажите корректный номер!' }]}>
+        <Input
+          name='phone'
+          className='form_input'
+          prefix={<LockOutlined className='site-form-item-icon' />}
+          type='phone'
+          placeholder='Укажите ваш сотовый'
+          onChange={inputChangeHandler}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button type='primary' htmlType='submit' className='login-form-button'>
+          Регистрация
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
 
