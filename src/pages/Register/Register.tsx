41321873052e063b8efc00ffa9a083c3e294c681
Dauren3310 <daur3310@gmail.com>
2021-07-06 01:28:52 +0600

    front-feature #74 Added 3 feilds: username, userSurname and phone in form Register

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,15 +1,20 @@
-import { Form, Input, Button } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import './Register.scss';
-import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined, PhoneOutlined, MailOutlined } from '@ant-design/icons';
+
+import './Register.scss';
 import { useActions } from '../../hooks/useAction';
 
 const Register: React.FC = () => {
   const [state, setState] = useState({
+    username: '',
+    userSurname: '',
+    phone: '',
     email: '',
     password: '',
   });
+
   const { signUpUser } = useActions();
 
   const submitFormHandler = () => {
@@ -22,10 +27,42 @@ const Register: React.FC = () => {
       return { ...prevState, [name]: value };
     });
   };
+
   return (
     <div className='Form'>
       <Form name='normal_login' className='login-form' initialValues={{ remember: true }} onFinish={submitFormHandler}>
         <h2 className='Form-header'>Регистрация</h2>
+
+        <Form.Item name='username' rules={[{ required: true, message: 'Пожалуйста, укажите ваше имя!' }]}>
+          <Input
+            name='username'
+            className='form_input'
+            prefix={<UserOutlined className='site-form-item-icon' />}
+            placeholder='Укажите ваше имя'
+            onChange={inputChangeHandler}
+          />
+        </Form.Item>
+
+        <Form.Item name='userSurname' rules={[{ required: true, message: 'Пожалуйста, укажите ваше имя!' }]}>
+          <Input
+            name='userSurname'
+            className='form_input'
+            prefix={<UserOutlined className='site-form-item-icon' />}
+            placeholder='Укажите вашу фамилию'
+            onChange={inputChangeHandler}
+          />
+        </Form.Item>
+
+        <Form.Item name='phone' rules={[{ required: true, message: 'Пожалуйста, укажите ваш номер телефона!' }]}>
+          <Input
+            name='phone'
+            className='form_input'
+            prefix={<PhoneOutlined className='site-form-item-icon' />}
+            placeholder='Укажите ваш номер телефона'
+            onChange={inputChangeHandler}
+          />
+        </Form.Item>
+
         <Form.Item
           name='email'
           rules={[{ type: 'email', required: true, message: 'Пожалуйста, укажите верный email!' }]}
@@ -33,7 +70,7 @@ const Register: React.FC = () => {
           <Input
             name='email'
             className='form_input'
-            prefix={<UserOutlined className='site-form-item-icon' />}
+            prefix={<MailOutlined className='site-form-item-icon' />}
             placeholder='Укажите ваш email'
             onChange={inputChangeHandler}
           />
