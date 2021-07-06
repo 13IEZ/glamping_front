bceb212f426ae18a047535a27a321aa71f626afe
Pavel Khegay <pashulia.pk@gmail.com>
2021-07-06 13:23:30 +0600

    front feature #68 added sorts

--- src/pages/ModulesList/ModulesList.tsx
+++ src/pages/ModulesList/ModulesList.tsx
@@ -1,21 +1,50 @@
 import React from 'react';
 
-import { Pagination } from 'antd';
+import { Pagination, Select } from 'antd';
 
 import ModuleCard from '../../components/ModuleCard/ModuleCard';
 
-const ModulesList: React.FC = () => {
+const { Option, OptGroup } = Select;
 
+function handleChange(value: any) {
+  console.log(`selected ${value}`);
+}
+
+const ModulesList: React.FC = () => {
   return (
-    <div className="Reviews">
+    <div className='ModulesList'>
+      <Select defaultValue='tent' style={{ width: 200 }} onChange={handleChange}>
+        <OptGroup label='Type'>
+          <Option value='tent'>Тент</Option>
+          <Option value='module'>Модуль</Option>
+          <Option value='multiframe'>Мультифрейм</Option>
+          <Option value='geocupol'>Геокупол</Option>
+          <Option value='binishell'>Бинишелл</Option>
+          <Option value='ortocupol'>Ортокупол</Option>
+          <Option value='yurt'>Юрта</Option>
+          <Option value='camper'>Автодом</Option>
+          <Option value='caravan'>Прицепной автодом</Option>
+        </OptGroup>
+      </Select>
+      <Select defaultValue='name' style={{ width: 200 }} onChange={handleChange}>
+        <OptGroup label='Name'>
+          <Option value='name'>Name</Option>
+        </OptGroup>
+      </Select>
+      <Select defaultValue='expensive' style={{ width: 200 }} onChange={handleChange}>
+        <OptGroup label='Price'>
+          <Option value='cheap'>Сначала дешёвые</Option>
+          <Option value='expensive'>Сначала дорогие</Option>
+        </OptGroup>
+      </Select>
       <ModuleCard />
       <ModuleCard />
       <ModuleCard />
-      <div className="example">
-          <Pagination defaultCurrent={1} total={50} showSizeChanger />
+      <div className='example'>
+        <Pagination defaultCurrent={1} total={50} showSizeChanger />
       </div>
     </div>
-  )
+  );
 };
 
 export default ModulesList;
