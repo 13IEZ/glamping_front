9ce0c707c4a99ab3e97088e864c79c2c9cfdb17b
Diana Isseyeva <cherepashka.d74@gmail.com>
2021-07-11 14:48:14 +0600

    front-feature #88 in folder Main created folder components and removed folders AboutUs, Investor, Landlord, ReviewsMini

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import AboutUs from '../../components/AboutUs/AboutUs';
-import Investor from '../../components/Investor/Investor';
-import Landlord from '../../components/Landlord/Landlord';
-import ReviewsMini from '../../components/ReviewsMini/ReviewsMini';
+import AboutUs from './components/AboutUs/AboutUs';
+import Investor from './components/Investor/Investor';
+import Landlord from './components/Landlord/Landlord';
+import ReviewsMini from './components/ReviewsMini/ReviewsMini';
 import CustomCarousel from '../../components/UI/Carousel/CustomCarousel';
 import CustomFooter from '../../components/UI/CustomFooter/CustomFooter';
 
