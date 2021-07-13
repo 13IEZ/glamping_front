0d65339b70c0e4876800f26e8ffae2d0777fdd82
Pavel Khegay <pashulia.pk@gmail.com>
2021-07-14 02:33:12 +0600

    front-bug #111 fixed adaptability bug

--- src/components/UI/CustomFooter/CustomFooter.tsx
+++ src/components/UI/CustomFooter/CustomFooter.tsx
@@ -12,48 +12,45 @@ const { Footer } = Layout;
 const CustomFooter: React.FC = () => {
   return (
     <Footer className='footer'>
-      <div className='container'>
-        <Row>
-          <Col className='footer-container__item' flex='auto'>
-            <List split={false}>
-              <List.Item>
-                <Link to='/investor'>Инвесторам </Link>
-              </List.Item>
-              <List.Item>
-                <Link to='/tourists'>Туристам</Link>
-              </List.Item>
-              <List.Item>
-                <Link to='/landlords'>Арендодателям</Link>
-              </List.Item>
-            </List>
-          </Col>
-          <Col className='footer-container__item' flex='auto'>
+      <Row>
+        <Col className='footer-container__item' flex='auto'>
+          <List split={false}>
             <List.Item>
-              Адрес: <a href='#'>Казахстан, г. Алматы, пр. Жибек Жолы, 135/10А, офис 514</a>
+              <Link to='/investor'>Инвесторам </Link>
             </List.Item>
             <List.Item>
-              Телефон: <a href='#'>+7-707-777-77-77</a>
+              <Link to='/tourists'>Туристам</Link>
             </List.Item>
             <List.Item>
-              Email: <a href='#'>example@mail.ru</a>
+              <Link to='/landlords'>Арендодателям</Link>
             </List.Item>
-          </Col>
-          <Col className='footer-container__item' flex='auto'>
-            <Space size='large'>
-              <a href='#'>
-                <FacebookFilled style={{ fontSize: '32px' }} />
-              </a>
-
-              <a href='#'>
-                <InstagramFilled style={{ fontSize: '32px' }} />
-              </a>
-            </Space>
-          </Col>
-        </Row>
-        <Row justify='center' style={{ marginTop: '10px' }}>
-          <Typography> &copy; 2021 Все права защищены </Typography>
-        </Row>
-      </div>
+          </List>
+        </Col>
+        <Col className='footer-container__item' flex='auto'>
+          <List.Item>
+            Адрес: <a href='#'>Казахстан, г. Алматы, пр. Жибек Жолы, 135/10А, офис 514</a>
+          </List.Item>
+          <List.Item>
+            Телефон: <a href='#'>+7-707-777-77-77</a>
+          </List.Item>
+          <List.Item>
+            Email: <a href='#'>example@mail.ru</a>
+          </List.Item>
+        </Col>
+        <Col className='footer-container__item' flex='auto'>
+          <Space size='large'>
+            <a href='#'>
+              <FacebookFilled style={{ fontSize: '32px' }} />
+            </a>
+            <a href='#'>
+              <InstagramFilled style={{ fontSize: '32px' }} />
+            </a>
+          </Space>
+        </Col>
+      </Row>
+      <Row justify='center' style={{ marginTop: '10px' }}>
+        <Typography> &copy; 2021 Все права защищены </Typography>
+      </Row>
     </Footer>
   );
 };
