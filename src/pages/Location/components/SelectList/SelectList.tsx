8c9a074ccca2dc7844c6c7829a72d86344cad9af
Mikhail Konev <mkonev2020@gmail.com>
2021-07-20 20:50:49 +0600

    front-enhancement #132 add functionality to the Yandex map

--- src/pages/Location/components/SelectList/SelectList.tsx
+++ src/pages/Location/components/SelectList/SelectList.tsx
@@ -3,11 +3,25 @@ import { Select } from 'antd';
 const { Option } = Select;
 import './SelectList.scss';
 
+const sortCoordinates = (arr: any) => {
+  const result = arr.sort(function (a: any, b: any) {
+    if (a.hint < b.hint) {
+      return -1;
+    }
+    if (a.hint > b.hint) {
+      return 1;
+    }
+    return 0;
+  });
+  return result;
+};
+
 const SelectList: React.FC<any> = props => {
+  const coordinates = sortCoordinates(props.coordinates);
   return (
     <div className='selectList-container'>
       <Select onChange={props.handleChange} className='selectList'>
-        {props.coordinates.map((coordinate: any) => (
+        {coordinates.map((coordinate: any) => (
           <Option key={coordinate.coordId} value={coordinate.coordId}>
             {coordinate.hint}
           </Option>
