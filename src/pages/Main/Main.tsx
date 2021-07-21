d3fb048e25702b3c3f620045abdeb2fde9feb800
Dauren3310 <daur3310@gmail.com>
2021-07-22 00:03:44 +0600

    front-enhancement #138 Resolved conflict in Main.tsx

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import AboutUs from './components/AboutUs/AboutUs';
-// import Investor from './components/Investor/Investor';
 import LocationMini from './components/LocationMini/LocationMini';
 import StoreMini from './components/StoreMini/StoreMini';
 import Landlord from './components/Landlord/Landlord';
@@ -14,7 +13,6 @@ const Main: React.FC = () => {
     <>
       <CustomCarousel />
       <AboutUs />
-      {/*<Investor />*/}
       <LocationMini />
       <StoreMini />
       <Landlord />
