26aee84092511794f242c90be078a57a4dc9ba8a
Pavel Khegay <pashulia.pk@gmail.com>
2021-07-22 14:38:52 +0600

    updated the look of the carousel component

--- src/components/UI/Carousel/CustomCarousel.tsx
+++ src/components/UI/Carousel/CustomCarousel.tsx
@@ -1,33 +1,73 @@
-import { Carousel, Button, Typography } from 'antd';
-const { Paragraph } = Typography;
 import './CustomCarousel.scss';
+
 import React from 'react';
+
+import { Button, Carousel } from 'antd';
+import { Link } from 'react-router-dom';
+
+import money from '../../../assets/icons/money-icon.png';
+import track from '../../../assets/icons/track-icon.png';
 
 const CustomCarousel: React.FC = () => {
   return (
     <section>
-      <Carousel draggable>
-        <div>
-          <h3 className='contentStyle first-slide'>
-            <Button type='primary'>Пример кнопки</Button>
-          </h3>
+      <Carousel autoplay>
+        <div className='contentStyle first-slide'>
+          <div className='content-slider'>
+            <div className='title-carousel'>GLAMPING</div>
+            <div className='text-carousel'>
+              Закажи <span className='yellowText'>glam-модуль</span> и остановись в самом красивом месте Казахстана
+              <div className='icon-text'>
+                <img className='icon' src={money} />
+                <span className='text'>отдыхай или сдавай</span>
+              </div>
+              <div className='icon-text'>
+                <img className='icon' src={track} />
+                <span className='text'>меняй локацию</span>
+              </div>
+              <Button className='carousel_button' style={{ background: '#FDD74E', border: '1px solid black' }}>
+                <Link to='/store'>Заказать модуль</Link>
+              </Button>
+            </div>
+          </div>
         </div>
-        <div>
-          <h3 className='contentStyle second-slide'>
-            <Paragraph style={{ color: '#fff' }}>Пример текста отображаемоего на слайде</Paragraph>
-          </h3>
+        <div className='contentStyle second-slide'>
+          <div className='content-slider'>
+            <div className='title-carousel'>GLAMPING</div>
+            <div className='text-carousel'>
+              Дополнительный доход от своего земельного участка!
+              <div className='icon-text'>
+                <img className='icon' src={money} />
+                <span className='text'>дополнительный доход</span>
+              </div>
+              <div className='icon-text'>
+                <img className='icon' src={track} />
+                <span className='text'>оперативное управление</span>
+              </div>
+              <Button className='carousel_button' style={{ background: '#FDD74E', border: '1px solid black' }}>
+                <Link to='#'>Сдать участок</Link>
+              </Button>
+            </div>
+          </div>
         </div>
-        <div>
-          <h3 className='contentStyle third-slide'>
-            <Paragraph style={{ color: '#fff' }}>Пример текста отображаемоего на слайде</Paragraph>
-            <Button>Пример кнопки</Button>
-          </h3>
-        </div>
-        <div>
-          <h3 className='contentStyle fourth-slide'>
-            <Button>Пример кнопки</Button>
-            <Paragraph style={{ color: '#fff' }}>Пример текста отображаемоего на слайде</Paragraph>
-          </h3>
+        <div className='contentStyle third-slide'>
+          <div className='content-slider'>
+            <div className='title-carousel'>GLAMPING</div>
+            <div className='text-carousel'>
+              Глэмпинг – это отдых на природе с пометкой "люкс"!
+              <div className='icon-text'>
+                <img className='icon' src={money} />
+                <span className='text'>особый формат отдыха</span>
+              </div>
+              <div className='icon-text'>
+                <img className='icon' src={track} />
+                <span className='text'>гламурный кемпинг</span>
+              </div>
+              <Button className='carousel_button' style={{ background: '#FDD74E', border: '1px solid black' }}>
+                <Link to='#'>Забронировать</Link>
+              </Button>
+            </div>
+          </div>
         </div>
       </Carousel>
     </section>
