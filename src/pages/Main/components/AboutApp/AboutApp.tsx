e71ca4db7aab09dfd4735e0160f83a998603c126
Pavel Khegay <pashulia.pk@gmail.com>
2021-07-29 02:06:36 +0600

    front-enhancement #171 changed the aboutapp block

--- src/pages/Main/components/AboutApp/AboutApp.tsx
+++ src/pages/Main/components/AboutApp/AboutApp.tsx
@@ -34,24 +34,50 @@ const AboutApp: React.FC = () => {
             </Card>
           </Col>
           <Col span={6}>
-            <Card hoverable style={{ width: 250 }} className='test2'>
-              <p className='content_text'>
+            <Card
+              hoverable
+              style={{ width: 250 }}
+              cover={
+                <img
+                  alt='example'
+                  src='https://obyektiv.press/sites/default/files/articles/c7e0b49947b49aa6256de78cf5ae84b6.jpg'
+                />
+              }
+            >
+              <p>
                 Раздел <Link to='/store'>Магазин</Link> имеет широкий выбор жилых модулей на любой вкус, цвет, размер и
                 кошелёк и готов предложить вам купить или продать жилой модуль!
               </p>
             </Card>
           </Col>
           <Col span={6}>
-            <Card className='test1' hoverable style={{ width: 250 }}>
-              <p className='content_text'>
+            <Card
+              hoverable
+              style={{ width: 250 }}
+              cover={
+                <img
+                  alt='example'
+                  src='https://obyektiv.press/sites/default/files/articles/c7e0b49947b49aa6256de78cf5ae84b6.jpg'
+                />
+              }
+            >
+              <p>
                 В разделе <Link to='/location'>Локации</Link> вы найдете огромную базу данных с участками для удобного
                 расположения вашего модуля в самых красивых местах Казахстана!
               </p>
             </Card>
           </Col>
-
           <Col span={6}>
-            <Card hoverable style={{ width: 250 }}>
+            <Card
+              hoverable
+              style={{ width: 250 }}
+              cover={
+                <img
+                  alt='example'
+                  src='https://obyektiv.press/sites/default/files/articles/c7e0b49947b49aa6256de78cf5ae84b6.jpg'
+                />
+              }
+            >
               <p>
                 В разделе <Link to='#'>Инвестировать</Link> наша команда профессионалов поможет разработать концепт и
                 бизнес-план глэмпинга, найти земельный участок и финансирование.
