5c7b20e633eaf82b3014729866d9158affa2a707
Pavel Khegay <pashulia.pk@gmail.com>
2021-07-29 13:36:57 +0600

    front-feature #150 added markup for the module sale form

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,16 @@ import { Route, Switch } from 'react-router';
 import AppToolbar from './components/UI/AppToolbar/AppToolbar';
 import ProtectedRoute from './helpers/ProtectedRoute';
 import { useTypedSelectorHook } from './hooks/useTypedSelector';
+import GLMapForm from './pages/Location/GLMap';
+import MapLocation from './pages/Location/MapLocation';
 import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import Register from './pages/Register/Register';
 import ReviewForm from './pages/ReviewForm/ReviewForm';
 import Reviews from './pages/Reviews/Reviews';
+import NewProduct from './pages/Store/components/NewProduct/NewProduct';
 import Store from './pages/Store/Store';
-import GLMapForm from './pages/Location/GLMap';
-import MapLocation from './pages/Location/MapLocation';
 
 function App(): JSX.Element {
   const { user } = useTypedSelectorHook(state => state.users);
@@ -29,6 +30,7 @@ function App(): JSX.Element {
         <Route path='/reviews' exact component={Reviews} />
         <ProtectedRoute isAllowed={!!user} path='/reviews/new' exact redirectTo={'/login'} component={ReviewForm} />
         <Route path='/store' exact component={Store} />
+        <ProtectedRoute isAllowed={!!user} path='/store/new' exact redirectTo={'/login'} component={NewProduct} />
         <Route path='/store/:id' exact component={ProductDetail} />
         <Route path='/location' exact component={GLMapForm} />
         <Route path='/maplocation/:hint' exact component={MapLocation} />
