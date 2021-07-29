bfe085f8177640ff4edfcaf8a6d368b287370b2b
Pavel Khegay <pashulia.pk@gmail.com>
2021-07-29 19:51:27 +0600

    front-bug #176 fixed bug

--- src/hooks/useAction.ts
+++ src/hooks/useAction.ts
@@ -1,24 +1,16 @@
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import * as LocationsActionCreators from '../store/actions/locationsAction';
-
 import * as CategoriesActionCreators from '../store/actions/categoriesAction';
-
+import * as LocationsActionCreators from '../store/actions/locationsAction';
 import * as ProductActionCreators from '../store/actions/productsAction';
-import * as StoreActionCreator from '../store/actions/storeAction';
 import * as UserActionCreators from '../store/actions/usersAction';
-
 
 const ActionCreators = {
   ...UserActionCreators,
   ...ProductActionCreators,
   ...LocationsActionCreators,
-
   ...CategoriesActionCreators,
-
-  ...StoreActionCreator,
-
 };
 
 export const useActions = (): any => {
