a3588a6a4c98ea266372cf9bb6d5e616e2b78997
Pavel Khegay <pashulia.pk@gmail.com>
2021-08-03 00:13:46 +0600

    front-enhancement #185 added responsiveness of the page and hiding pagination if there are less than 2 pages

--- src/pages/Location/components/GLMap/GLMap/GLMap.tsx
+++ src/pages/Location/components/GLMap/GLMap/GLMap.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { useHistory } from 'react-router-dom';
 import { Clusterer, Map, Placemark, YMaps } from 'react-yandex-maps';
 
--- src/pages/Store/components/StoreSidebar/StoreSidebar.tsx
+++ src/pages/Store/components/StoreSidebar/StoreSidebar.tsx
@@ -11,43 +11,45 @@ const { SubMenu } = Menu;
 const StoreSidebar: React.FC = () => {
   const { fetchModulesCat } = useActions();
   return (
-    <Sider width={180} className='site-layout-background'>
-      <Menu mode='inline' style={{ height: '100%', borderRight: 0 }}>
-        <SubMenu key='sub1' title='Цена'>
-          <Menu.Item key={uuidv4()} onClick={() => fetchModulesCat('price', 'asc')}>
-            По возрастанию
-          </Menu.Item>
-          <Menu.Item key={uuidv4()} onClick={() => fetchModulesCat('price', 'desc')}>
-            По убыванию
-          </Menu.Item>
-        </SubMenu>
-        <SubMenu key='sub2' title='Вместимость'>
-          <Menu.Item key={uuidv4()}>1-о местный</Menu.Item>
-          <Menu.Item key={uuidv4()}>2-ух местный</Menu.Item>
-          <Menu.Item key={uuidv4()}>3-ех местный</Menu.Item>
-          <Menu.Item key={uuidv4()}>5-и и более местный</Menu.Item>
-        </SubMenu>
-        <SubMenu key='sub3' title='Сезоность'>
-          <Menu.Item key={uuidv4()}>Лето</Menu.Item>
-          <Menu.Item key={uuidv4()}>Зима</Menu.Item>
-        </SubMenu>
-        <SubMenu key='sub4' title='Производитель'>
-          <Menu.Item key={uuidv4()}>Производитель 1</Menu.Item>
-          <Menu.Item key={uuidv4()}>Производитель 2</Menu.Item>
-          <Menu.Item key={uuidv4()}>Производитель 3</Menu.Item>
-        </SubMenu>
-        <SubMenu key='sub5' title='Категории'>
-          <Menu.Item key={uuidv4()}>Тенты</Menu.Item>
-          <Menu.Item key={uuidv4()}>Каркасные дома</Menu.Item>
-          <Menu.Item key={uuidv4()}>Купола</Menu.Item>
-          <Menu.Item key={uuidv4()}>Юрты</Menu.Item>
-          <Menu.Item key={uuidv4()}>Модули</Menu.Item>
-          <Menu.Item key={uuidv4()}>Трейлеры (прицепы)</Menu.Item>
-          <Menu.Item key={uuidv4()}>Автодома</Menu.Item>
-          <Menu.Item key={uuidv4()}>Другое</Menu.Item>
-        </SubMenu>
-      </Menu>
-    </Sider>
+    <>
+      <Sider className='site-layout-background' width={200}>
+        <Menu mode='inline' style={{ height: '100%', borderRight: 0 }}>
+          <SubMenu key='sub1' title='Цена'>
+            <Menu.Item key={uuidv4()} onClick={() => fetchModulesCat('price', 'asc')}>
+              По возрастанию
+            </Menu.Item>
+            <Menu.Item key={uuidv4()} onClick={() => fetchModulesCat('price', 'desc')}>
+              По убыванию
+            </Menu.Item>
+          </SubMenu>
+          <SubMenu key='sub2' title='Вместимость'>
+            <Menu.Item key={uuidv4()}>1-о местный</Menu.Item>
+            <Menu.Item key={uuidv4()}>2-ух местный</Menu.Item>
+            <Menu.Item key={uuidv4()}>3-ех местный</Menu.Item>
+            <Menu.Item key={uuidv4()}>5-и и более местный</Menu.Item>
+          </SubMenu>
+          <SubMenu key='sub3' title='Сезоность'>
+            <Menu.Item key={uuidv4()}>Лето</Menu.Item>
+            <Menu.Item key={uuidv4()}>Зима</Menu.Item>
+          </SubMenu>
+          <SubMenu key='sub4' title='Производитель'>
+            <Menu.Item key={uuidv4()}>Производитель 1</Menu.Item>
+            <Menu.Item key={uuidv4()}>Производитель 2</Menu.Item>
+            <Menu.Item key={uuidv4()}>Производитель 3</Menu.Item>
+          </SubMenu>
+          <SubMenu key='sub5' title='Категории'>
+            <Menu.Item key={uuidv4()}>Тенты</Menu.Item>
+            <Menu.Item key={uuidv4()}>Каркасные дома</Menu.Item>
+            <Menu.Item key={uuidv4()}>Купола</Menu.Item>
+            <Menu.Item key={uuidv4()}>Юрты</Menu.Item>
+            <Menu.Item key={uuidv4()}>Модули</Menu.Item>
+            <Menu.Item key={uuidv4()}>Трейлеры (прицепы)</Menu.Item>
+            <Menu.Item key={uuidv4()}>Автодома</Menu.Item>
+            <Menu.Item key={uuidv4()}>Другое</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </Sider>
+    </>
   );
 };
 
