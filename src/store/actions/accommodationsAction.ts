7c68b4c7d4a001be5f06562790a65584d27cf82b
Diana Isseyeva <cherepashka.d74@gmail.com>
2021-08-06 20:56:40 +0600

    front-feature #186 changed accommodationsAction

--- src/store/actions/accommodationsAction.ts
+++ src/store/actions/accommodationsAction.ts
@@ -5,10 +5,11 @@ import { Dispatch } from 'redux';
 import ax from '../../settings/axios-glamping';
 import { AccommodationsAction, AccommodationsActionTypes } from '../types/accommodationsTypes';
 
+// get all accommodations related to current location
 export const fetchAccommodations = (currentLocationId: string): any => {
   return async (dispatch: Dispatch<AccommodationsAction>) => {
     try {
-      const response = await ax.get(`/accommodations/?location=${currentLocationId}`);
+      const response = await ax.get(`/accommodations/${currentLocationId}`);
       dispatch({ type: AccommodationsActionTypes.FETCH_ACCOMMODATIONS_SUCCESS, payload: response.data });
     } catch (error) {
       dispatch({
