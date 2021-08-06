9d39152bc33898f81da596c6ff003403123ca40d
Diana Isseyeva <cherepashka.d74@gmail.com>
2021-08-06 21:20:37 +0600

    front-feature #186 fixed conflict

--- src/pages/Main/components/LocationMini/components/LocationMiniItem.tsx
+++ src/pages/Main/components/LocationMini/components/LocationMiniItem.tsx
@@ -22,7 +22,7 @@ const LocationMiniItem: React.FC<locationCardProps> = ({ title, rent, square, im
 
   return (
     <div>
-      <Link to={`/locations/${_id}`}>
+      <Link to={`/location/${_id}`}>
         <Card className='item__card' cover={<img alt='Not found' src={cardImage} className='item__card-img' />}>
           <Meta style={{ marginTop: 22, fontSize: 24, textAlign: 'center' }} title={title} />
           <p className='item__square'>{square} соток</p>
