63f65d7817049564ec474110a14a8aa675ca7751
Mikhail Konev <mkonev2020@gmail.com>
2021-08-11 20:57:48 +0600

    front-feature #208 add front jenkins CI/CD settings

--- src/settings/config.ts
+++ src/settings/config.ts
@@ -1,3 +1,10 @@
+const port = 8000;
+let host = 'http://localhost';
+
+if (process.env.REACT_APP_CI) {
+  host = 'http://glamping-ci.ddns.net';
+}
+
 export default {
-  apiUrl: 'http://localhost:8000',
+  apiUrl: host + ':' + port,
 };
