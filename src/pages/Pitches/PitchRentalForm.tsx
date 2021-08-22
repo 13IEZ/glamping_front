a617781856b37dba2da1680d41560b82d1b6c98c
Pavel Khegay <pashulia.pk@gmail.com>
2021-08-22 17:20:31 +0600

    made changes to the form

--- src/pages/Pitches/PitchRentalForm.tsx
+++ src/pages/Pitches/PitchRentalForm.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { Button, Col, Form, Input, InputNumber, Row, Select, Typography } from 'antd';
+import { Button, Col, Divider, Form, Input, InputNumber, Row, Select, Typography } from 'antd';
 
 import { useActions } from '../../hooks/useAction';
 import { useTypedSelectorHook } from '../../hooks/useTypedSelector';
@@ -21,11 +21,15 @@ const formTailLayout = {
 const { Option } = Select;
 const { TextArea } = Input;
 const { Title } = Typography;
+
 const PitchRentalForm: React.FC = (props: any) => {
   const idCurrentPitch = props.match.params.id;
   const { currentPitch } = useTypedSelectorHook(state => state.piches);
   const { pitchReservations } = useTypedSelectorHook(state => state.reservations);
   const { fetchCurrentPitch } = useActions();
+  const config = {
+    rules: [{ type: 'object' as const, required: true, message: 'Укажите дату изготовления!' }],
+  };
 
   useEffect(() => {
     fetchPitchReservations(idCurrentPitch);
@@ -88,121 +92,122 @@ const PitchRentalForm: React.FC = (props: any) => {
             />
           </Col>
         </Row>
-        <Form form={form} name='dynamic_rule' onFinish={submitFormHandler}>
-          <Title level={4}>Опишите ваш модуль</Title>
-          <Form.Item
-            {...formItemLayout}
-            name='title'
-            label='Название'
-            rules={[
-              {
-                required: true,
-                message: 'Укажите название!',
-              },
-            ]}
-          >
-            <Input onChange={inputChangeHandler} placeholder='Введите название модуля' />
-          </Form.Item>
-          <Form.Item
-            {...formItemLayout}
-            name='category'
-            label='Категория'
-            rules={[{ required: true, message: 'Укажите категорию!' }]}
-          >
-            <Select placeholder='Выберите категорию'>
-              <Option onChange={inputChangeHandler} value='Тенты'>
-                Тенты
-              </Option>
-              <Option onChange={inputChangeHandler} value='Автодома'>
-                Автодома
-              </Option>
-              <Option onChange={inputChangeHandler} value='Каркасный дом'>
-                Каркасный дом
-              </Option>
-              <Option onChange={inputChangeHandler} value='Купола'>
-                Купола
-              </Option>
-              <Option onChange={inputChangeHandler} value='Модули'>
-                Модули
-              </Option>
-              <Option onChange={inputChangeHandler} value='Юрты'>
-                Юрты
-              </Option>
-              <Option onChange={inputChangeHandler} value='Трейлеры'>
-                Трейлеры
-              </Option>
-              <Option onChange={inputChangeHandler} value='Другое'>
-                Другое
-              </Option>
-            </Select>
-          </Form.Item>
-          <Form.Item
-            {...formItemLayout}
-            name='season'
-            label='Сезоность'
-            rules={[{ required: true, message: 'Укажите сезонность!' }]}
-          >
-            <Select placeholder='Выберите сезонность'>
-              <Option onChange={inputChangeHandler} value='summer'>
-                Лето
-              </Option>
-              <Option onChange={inputChangeHandler} value='summer'>
-                Зима
-              </Option>
-              <Option onChange={inputChangeHandler} value='all'>
-                Всесезонный
-              </Option>
-            </Select>
-          </Form.Item>
-          <Form.Item
-            {...formItemLayout}
-            name='roominess'
-            label='Вместимость'
-            rules={[
-              {
-                required: true,
-                message: 'Укажите вместимость!',
-              },
-            ]}
-          >
-            <InputNumber style={{ width: '100%' }} placeholder='Укажите вместимость в цифрах' />
-          </Form.Item>
-          <Form.Item
-            {...formItemLayout}
-            label='Цена аренды за сутки'
-            name='rent'
-            rules={[
-              {
-                required: true,
-                message: 'Укажите цену аренды!',
-              },
-            ]}
-          >
-            <InputNumber style={{ width: '100%' }} placeholder='12345...' />
-          </Form.Item>
-          <Form.Item
-            {...formItemLayout}
-            name='description'
-            label='Описание'
-            rules={[
-              {
-                required: true,
-                message: 'Укажите описание!',
-              },
-            ]}
-          >
-            <TextArea rows={4} name='Note' className='form_input' placeholder='Опишите ваш модуль' />
-          </Form.Item>
-          <Form.Item>
-            <Calendar pitchId={currentPitch._id} reservedDates={reservedDates} />
-          </Form.Item>
-          <Form.Item {...formTailLayout}>
-            <Button type='primary' onClick={onCheck}>
-              Подать заявку
-            </Button>
-          </Form.Item>
-        </Form>
       </div>
+      <Divider />
+      <Form form={form} name='dynamic_rule' onFinish={submitFormHandler} className='PitchForm'>
+        <Title level={3}>Опишите ваш модуль для заявки</Title>
+        <Form.Item
+          {...formItemLayout}
+          name='title'
+          label='Название'
+          rules={[
+            {
+              required: true,
+              message: 'Укажите название!',
+            },
+          ]}
+        >
+          <Input onChange={inputChangeHandler} placeholder='Введите название модуля' />
+        </Form.Item>
+        <Form.Item
+          {...formItemLayout}
+          name='category'
+          label='Категория'
+          rules={[{ required: true, message: 'Укажите категорию!' }]}
+        >
+          <Select placeholder='Выберите категорию'>
+            <Option onChange={inputChangeHandler} value='Тенты'>
+              Тенты
+            </Option>
+            <Option onChange={inputChangeHandler} value='Автодома'>
+              Автодома
+            </Option>
+            <Option onChange={inputChangeHandler} value='Каркасный дом'>
+              Каркасный дом
+            </Option>
+            <Option onChange={inputChangeHandler} value='Купола'>
+              Купола
+            </Option>
+            <Option onChange={inputChangeHandler} value='Модули'>
+              Модули
+            </Option>
+            <Option onChange={inputChangeHandler} value='Юрты'>
+              Юрты
+            </Option>
+            <Option onChange={inputChangeHandler} value='Трейлеры'>
+              Трейлеры
+            </Option>
+            <Option onChange={inputChangeHandler} value='Другое'>
+              Другое
+            </Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          {...formItemLayout}
+          name='season'
+          label='Сезоность'
+          rules={[{ required: true, message: 'Укажите сезонность!' }]}
+        >
+          <Select placeholder='Выберите сезонность'>
+            <Option onChange={inputChangeHandler} value='summer'>
+              Лето
+            </Option>
+            <Option onChange={inputChangeHandler} value='summer'>
+              Зима
+            </Option>
+            <Option onChange={inputChangeHandler} value='all'>
+              Всесезонный
+            </Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          {...formItemLayout}
+          name='roominess'
+          label='Вместимость'
+          rules={[
+            {
+              required: true,
+              message: 'Укажите вместимость!',
+            },
+          ]}
+        >
+          <InputNumber style={{ width: '100%' }} placeholder='Укажите вместимость в цифрах' />
+        </Form.Item>
+        <Form.Item
+          {...formItemLayout}
+          label='Цена аренды за сутки'
+          name='rent'
+          rules={[
+            {
+              required: true,
+              message: 'Укажите цену аренды!',
+            },
+          ]}
+        >
+          <InputNumber style={{ width: '100%' }} placeholder='12345...' />
+        </Form.Item>
+        <Form.Item
+          {...formItemLayout}
+          name='description'
+          label='Описание'
+          rules={[
+            {
+              required: true,
+              message: 'Укажите описание!',
+            },
+          ]}
+        >
+          <TextArea rows={4} name='Note' className='form_input' placeholder='Опишите ваш модуль' />
+        </Form.Item>
+        <Form.Item {...formItemLayout} name='date' label='Укажите даты брони' {...config}>
+          <Calendar pitchId={currentPitch._id} reservedDates={reservedDates} />
+        </Form.Item>
+        <Form.Item {...formTailLayout}>
+          <Button type='primary' onClick={onCheck}>
+            Подать заявку
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
