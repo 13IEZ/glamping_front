00fc2b5dfffdc9b2559d231c7cb436bbcd828231
Diana Isseyeva <cherepashka.d74@gmail.com>
2021-09-07 12:26:02 +0600

    front-bug #306 hide suggestion to create review on accomodation page

--- src/pages/CurrentAccommodation/CurrentAccommodation.tsx
+++ src/pages/CurrentAccommodation/CurrentAccommodation.tsx
@@ -76,8 +76,13 @@ const CurrentAccommodation: React.FC = (props: any) => {
         <Space direction='vertical' size={12}>
           <Calendar accommodationId={currentAccommodation._id} reservedDates={reservedDates} />
         </Space>
-        <Paragraph>Оставьте комментарий</Paragraph>
-        {user !== null && idCurrentAccommodation && <ReviewForm idCurrentAccommodation={idCurrentAccommodation} />}
+
+        {user !== null && idCurrentAccommodation && (
+          <>
+            <Paragraph>Оставьте комментарий</Paragraph>
+            <ReviewForm idCurrentAccommodation={idCurrentAccommodation} />
+          </>
+        )}
         <Title level={4}>Отзывы {currentAccommodation.reviewsQuantity}</Title>
         {reviewsList.length !== 0 ? (
           <>
